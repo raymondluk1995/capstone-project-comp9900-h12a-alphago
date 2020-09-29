29e37bdcfd9d053e4efd42f438087c378e3df095
Taria <taria8016@gmail.com>
2020-09-29 13:40:58 +1000

    axios

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="header">
+<<<<<<< Updated upstream
     <el-row type="flex" justify="space-around" style="align-items: center">
 <!--    <el-row :gutter="20">-->
     <el-col :span="8">
@@ -15,12 +16,27 @@
                 -->
         </div>
       </el-col>
+=======
+    <el-row type="flex" justify="center">
+        <el-col :span="16">
+            <el-row type="flex" justify="space-between" class="header-content">
+          <el-col :span="8">
+            <img @click="goto('home')" src="@/assets/Alogo.png"
+                 alt="render failure" style="width: 100px; height:90px; cursor: pointer">
+          </el-col>
+>>>>>>> Stashed changes
 
-<!--      <el-row type="flex" justify="center" style="align-items: center;">-->
-        <el-col :span="4">
-          <slot></slot>
+          <el-col :span="8">
+            <div class="slogan" >
+              <h4 style="font-family:Georgia;color: #222222">ALPHAGO AUCTION</h4>
+            </div>
+          </el-col>
+
+            <el-col :span="4">
+              <slot></slot>
+            </el-col>
+      </el-row>
         </el-col>
-<!--      </el-row>-->
     </el-row>
   </div>
 </template>
@@ -43,9 +59,15 @@ export default {
 <style scoped lang="scss">
 .header {
   /*margin-bottom: 30px;*/
-  margin-left: 50px;
-  margin-right: 50px;
+  margin-left: 15px;
+  margin-right: 15px;
   padding: 15px;
   border-bottom: 2px solid #f2f2f2;
+}
+.header-content {
+    .el-col {
+        display: flex;
+        align-items: center;
+    }
 }
 </style>
