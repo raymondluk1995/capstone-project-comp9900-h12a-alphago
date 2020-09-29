2df42b4148e4e5d8097d26e9abaa9c5f4e9a7cad
Taria <taria8016@gmail.com>
2020-09-29 13:49:50 +1000

    udate

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,13 +3,7 @@ import VueRouter from 'vue-router'
 import store from "../store"
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
-<<<<<<< Updated upstream
-// import Register from '../views/Register.vue'
-// import Resetpassword from "../views/Resetpassword";
-=======
-import Register from '../views/Register.vue'
-import Resetpassword from "../views/Resetpassword";
->>>>>>> Stashed changes
+
 
 Vue.use(VueRouter);
 
@@ -23,7 +17,7 @@ const routes = [{
     name: 'login',
     component: Login,
   },
-<<<<<<< Updated upstream
+
   // {
   //   path: '/register',
   //   name: 'register',
@@ -35,19 +29,6 @@ const routes = [{
   //   name: 'reset',
   //   component: Resetpassword,
   // }
-=======
-  {
-    path: '/register',
-    name: 'register',
-    component: Register,
-  }
-    ,
-  {
-    path: '/reset',
-    name: 'reset',
-    component: Resetpassword,
-  }
->>>>>>> Stashed changes
 ];
 
 const router = new VueRouter({
