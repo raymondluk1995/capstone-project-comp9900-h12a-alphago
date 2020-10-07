d420664d98cc5efc36f684e78dc8084ef4a69dc3
Taria <taria8016@gmail.com>
2020-10-07 15:03:05 +1100

    update

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,9 +5,9 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        username:'Taria',
+        username:'',
         jwt: '',
-        firstname:'Taria',
+        firstname:'',
         avatar:'',
     },
     getters: {
