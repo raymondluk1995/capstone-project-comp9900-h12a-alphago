ae168c50a64b4f3293f8f374befeae8a9648ed20
ansuzuka <19198737@qq.com>
2020-10-08 11:05:27 +0800

    Create User_profile.vue
    user_profile

---
+++ frontend/src/views/User_profile.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="user_profile">
+        <Header>
+        <el-button round type="primary" @click="back">Back</el-button> 
+        </Header>
+        
+    </div>
+</template>
+<script>
+import Header from "@/components/Header.vue";
+export default {
+  name: 'Home',
+  components: {
+    Header
+  },data() {
+    return {
+      flag: flase // 默认不显示返回按钮
+    };
+  },
+  methods: {
+      back() {
+        this.$router.go(-1);
+      },
+  }
+
+}
+</script>
