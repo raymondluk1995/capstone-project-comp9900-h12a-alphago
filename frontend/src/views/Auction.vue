16562ed3384b298de915f6a8788a491a519d3539
Taria <taria8016@gmail.com>
2020-10-14 19:14:07 +1100

    upup

--- frontend/src/views/Auction.vue
+++ frontend/src/views/Auction.vue
@@ -135,10 +135,9 @@
         },
         data() {
             return {
-                isEmpty: false,
+                isEmpty: true,
                 hasLogin: false,
-                propList: [{photos:['https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-                    'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png']}],
+                propList: [],
             }
         },
         methods: {
