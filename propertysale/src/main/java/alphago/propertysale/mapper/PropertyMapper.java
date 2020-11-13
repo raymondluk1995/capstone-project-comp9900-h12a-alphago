e3e7d13b8f8f682d4b996d067127f23c90116d76
zxhhaha <zxhhaha@gmail.com>
2020-11-13 17:04:03 +1100

    rm backend

--- propertysale/src/main/java/alphago/propertysale/mapper/PropertyMapper.java
+++
@@ -1,7 +0,0 @@
-package alphago.propertysale.mapper;
-
-import alphago.propertysale.entity.Property;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface PropertyMapper extends BaseMapper<Property> {
-}
--- propertysale/src/main/java/alphago/propertysale/service/AddressService.java
+++
@@ -1,7 +0,0 @@
-package alphago.propertysale.service;
-
-import alphago.propertysale.entity.Address;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface AddressService extends IService<Address> {
-}
