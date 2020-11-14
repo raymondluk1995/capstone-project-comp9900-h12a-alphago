458e601d621248efcfa5ab96e27606b5756e7836
zxhhaha <zxhhaha@gmail.com>
2020-11-15 00:14:45 +1100

    websocker debug

--- propertysale/src/test/java/alphago/propertysale/PropertysaleApplicationTests.java
+++ propertysale/src/test/java/alphago/propertysale/PropertysaleApplicationTests.java
@@ -7,14 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
 import java.util.List;
 
 //@SpringBootTest
 class PropertysaleApplicationTests {
-
 	@Test
 	void contextLoads() {
-
 	}
 
 }
